953d079819a1c5167593ca62fff003cbff97ce0c
Brent Mifsud <brentmifsud@icloud.com>
2019-06-16 18:17:41 -0400

    feat: add saving and deleting functionality to the note list view controller.

--- Mooskine/View Controllers/NotesListViewController.swift
+++ Mooskine/View Controllers/NotesListViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class NotesListViewController: UIViewController, UITableViewDataSource {
     /// A table view that displays a list of notes for a notebook
@@ -16,6 +17,8 @@ class NotesListViewController: UIViewController, UITableViewDataSource {
     var notebook: Notebook!
 
 	var notes: [Note] = []
+
+	var dataController: DataController!
 
     /// A date formatter for date text in note cells
     let dateFormatter: DateFormatter = {
@@ -29,6 +32,18 @@ class NotesListViewController: UIViewController, UITableViewDataSource {
 
         navigationItem.title = notebook.name
         navigationItem.rightBarButtonItem = editButtonItem
+
+		let fetchRequest: NSFetchRequest<Note> = Note.fetchRequest()
+		let sortDescriptor = NSSortDescriptor(key: "creationDate", ascending: false)
+		let predicate = NSPredicate(format: "notebook == %@", notebook)
+		fetchRequest.predicate = predicate
+		fetchRequest.sortDescriptors = [sortDescriptor]
+
+		if let result = try? dataController.viewContext.fetch(fetchRequest) {
+			notes = result
+			tableView.reloadData()
+		}
+
         updateEditButtonState()
     }
 
@@ -53,21 +68,43 @@ class NotesListViewController: UIViewController, UITableViewDataSource {
 
     // Adds a new `Note` to the end of the `notebook`'s `notes` array
     func addNote() {
-//		TODO: add note
-//        notebook.addNote()
-        tableView.insertRows(at: [IndexPath(row: numberOfNotes - 1, section: 0)], with: .fade)
-        updateEditButtonState()
+
+		let note = Note(context: dataController.viewContext)
+		note.text = "Example: Get Milk"
+		note.creationDate = Date()
+		note.notebook = notebook
+
+		do {
+			try dataController.viewContext.save()
+
+			notes.insert(note, at: 0)
+
+			tableView.insertRows(at: [IndexPath(row: 0, section: 0)], with: .fade)
+
+			updateEditButtonState()
+		} catch {
+			presentErrorAlert(title: "Unable to Add Note", message: "The following error occured:\n\(error.localizedDescription)")
+		}
     }
 
     // Deletes the `Note` at the specified index path
     func deleteNote(at indexPath: IndexPath) {
-//		TODO: remove note
-//		notebook.removeNote(at: indexPath.row)
-        tableView.deleteRows(at: [indexPath], with: .fade)
-        if numberOfNotes == 0 {
-            setEditing(false, animated: true)
-        }
-        updateEditButtonState()
+		let noteToDelete = note(at: indexPath)
+
+		dataController.viewContext.delete(noteToDelete)
+
+		do {
+			try dataController.viewContext.save()
+
+			notes.remove(at: indexPath.row)
+			tableView.deleteRows(at: [indexPath], with: .fade)
+			if numberOfNotes == 0 {
+				setEditing(false, animated: true)
+			}
+			updateEditButtonState()
+		} catch {
+			presentErrorAlert(title: "Unable to Delete Note", message: "The following error occured:\n\(error.localizedDescription)")
+		}
     }
 
     func updateEditButtonState() {
